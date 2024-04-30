0923f52b4084cea83e7e3f2a7435bc2ef39962f3
Faisal Bahari <fbhr1112@gmail.com>
2024-04-30 22:06:38 +0700

    feat: crud order

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,9 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('/v1')->group(function() {
+Route::prefix('/v1')->group(function () {
     include __DIR__ . '/team.php';
-    include __DIR__.'/tech-stack.php';
+    include __DIR__ . '/tech-stack.php';
     include __DIR__ . '/consultation.php';
+    include __DIR__ . '/order.php';
 });
